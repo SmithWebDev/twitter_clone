ddbc5cab747f739473389db64367c567e9e6e2d2
SmithWebDev <smithwebdd@gmail.com>
2021-04-14 09:56:10 -0400

    updated post controller with render message instead of format. using json no html responses

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,17 +11,28 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+#    respond_to do |format|
+#      if @post.save
+#        format.html { redirect_to @post, notice: "Post was successfully created." }
+#        format.json { render :show, status: :created, location: @post }
+#      else
+#        format.html { render :new }
+#        format.json { render json: @post.errors, status: :unprocessable_entity }
+#      end
+#    end
+#  end
+
+    if @post.save
+      render json: {
+        message: 'Post was successfully created.'
+      }, status: :created
+    else
+      render json: {
+        errors: @post.errors,
+        message: 'Post could not be created.'
+      }, status: :unprocessable_entity
     end
   end
-
   private
 
   # Only allow a list of trusted parameters through.
