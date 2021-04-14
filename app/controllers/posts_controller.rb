537623732bbdfd1c8cfd7f329480ebdd2ef712a3
SmithWebDev <smithwebdd@gmail.com>
2021-04-14 10:19:05 -0400

    removed scaffold save format, applied json conditional statement for save and error. added emit status for post helper, and added async wrapper to the post list partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,13 @@
 class PostsController < ApplicationController
   matestack_app TwitterClone::App
-  # GET /posts or /posts.json
+
   def index
-    # Remove instance variable, opting to call the actual index.rb file directly
-    # @posts = Post.all
     render TwitterClone::Pages::Posts::Index
   end
 
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
-
-#    respond_to do |format|
-#      if @post.save
-#        format.html { redirect_to @post, notice: "Post was successfully created." }
-#        format.json { render :show, status: :created, location: @post }
-#      else
-#        format.html { render :new }
-#        format.json { render json: @post.errors, status: :unprocessable_entity }
-#      end
-#    end
-#  end
 
     if @post.save
       render json: {
@@ -33,6 +20,7 @@ class PostsController < ApplicationController
       }, status: :unprocessable_entity
     end
   end
+
   private
 
   # Only allow a list of trusted parameters through.
--- app/matestack/twitter_clone/pages/posts/index.rb
+++ app/matestack/twitter_clone/pages/posts/index.rb
@@ -33,24 +33,23 @@ class TwitterClone::Pages::Posts::Index < Matestack::Ui::Page
     {
       for: Post.new, path: posts_path, method: :post,
       errors: {
-        wrapper: {
-          tag: :div,
-          class: 'invalid-feedback'
-        },
-        input: {
-          class: 'is-invalid'
-        }
-      }
+        wrapper: { tag: :div, class: 'invalid-feedback' },
+        input: { class: 'is-invalid' }
+      },
+      success: { emit: 'submitted' }
     }
   end
+
   def post_list_partial
-    @posts.each do |post|
-      div class: 'mb-3 p-3 rounded shadow-sm' do
-        heading size: 5 do
-          plain post.username
-          small text: post.created_at.strftime('%d.%m.%Y %H:%M')
+    async rerender_on: 'submitted', id: 'post-listed' do
+      @posts.each do |post|
+        div class: 'mb-3 p-3 rounded shadow-sm' do
+          heading size: 5 do
+            plain post.username
+            small text: post.created_at.strftime('%d.%m.%Y %H:%M')
+          end
+          paragraph text: post.body
         end
-        paragraph text: post.body
       end
     end
   end
