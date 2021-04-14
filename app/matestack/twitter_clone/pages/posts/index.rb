db9d81313309d15ea3c7d1e51cca98172955047b
SmithWebDev <smithwebdd@gmail.com>
2021-04-14 09:49:31 -0400

    updated the index.rb file with form, added partials to private for post form and post list

--- app/matestack/twitter_clone/pages/posts/index.rb
+++ app/matestack/twitter_clone/pages/posts/index.rb
@@ -6,6 +6,44 @@ class TwitterClone::Pages::Posts::Index < Matestack::Ui::Page
 
   # Describes how to show the posts (using bootstrap classes)
   def response
+    post_form_partial
+    post_list_partial
+  end
+
+  private
+
+  def post_form_partial
+    div class: 'mb-3 p-3 rounded shadow-sm' do
+      heading size: 4, text: 'New Post', class: 'mb-3'
+      matestack_form form_config_helper do
+        div class: 'mb-3' do
+          form_input key: :username, type: :text, placeholder: 'Username', class: 'form-control'
+        end
+        div class: 'mb-3' do
+          form_textarea key: :body,placeholder: "What's up?", class: 'form-control'
+        end
+        div class: 'mb-3' do
+          button 'submit', type: :submit, class: 'btn btn-primary', text: 'Post!'
+        end
+      end
+    end
+  end
+
+  def form_config_helper
+    {
+      for: Post.new, path: posts_path, method: :post,
+      errors: {
+        wrapper: {
+          tag: :div,
+          class: 'invalid-feedback'
+        },
+        input: {
+          class: 'is-invalid'
+        }
+      }
+    }
+  end
+  def post_list_partial
     @posts.each do |post|
       div class: 'mb-3 p-3 rounded shadow-sm' do
         heading size: 5 do
