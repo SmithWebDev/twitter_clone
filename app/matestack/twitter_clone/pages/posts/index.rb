42f9dbe644d282661fcad09a5b3f5e24f8a3b6b7
SmithWebDev <smithwebdd@gmail.com>
2021-04-14 10:40:23 -0400

    likes counter updating with like count on local window

--- app/matestack/twitter_clone/pages/posts/index.rb
+++ app/matestack/twitter_clone/pages/posts/index.rb
@@ -43,12 +43,23 @@ class TwitterClone::Pages::Posts::Index < Matestack::Ui::Page
   def post_list_partial
     async rerender_on: 'submitted', id: 'post-listed' do
       @posts.each do |post|
-        div class: 'mb-3 p-3 rounded shadow-sm' do
-          heading size: 5 do
-            plain post.username
-            small text: post.created_at.strftime('%d.%m.%Y %H:%M')
+        post_partial(post)
+      end
+    end
+  end
+
+  def post_partial(post)
+    async rerender_on: "liked_post_#{post.id}", id: "post-#{post.id}" do
+      div class: 'mb-3 p-3 rounded shadow-sm' do
+        heading size: 5 do
+          plain post.username
+          small text: post.created_at.strftime('%d.%m.%Y %H:%M')
+        end
+        paragraph text: post.body, class: 'mb-5'
+        action path: like_post_path(post), method: :put, success: {emit: "liked_post_#{post.id}"} do
+          button class: 'btn btn-light' do
+            plain "like (#{post.likes_count})"
           end
-          paragraph text: post.body
         end
       end
     end
